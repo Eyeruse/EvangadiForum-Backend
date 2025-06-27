19d144b63537ee218d14d7d767620e3f5b5cb20d
Eyeruse <eyerusalemg11@gmail.com>
2025-06-27 19:05:59 -0400

    Updated

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require ("dotenv").config()
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-const port = 5500;
-// database connection
-const dbConnection = require("./db/dbConfig")
+const port = process.env.PORT || 5500;
 
+// database connection
+const dbConnection = require("./db/dbConfig");
 
 // user routes file
 const userRoute = require("./routes/userRoute");
@@ -13,44 +14,40 @@ const userRoute = require("./routes/userRoute");
 const questionRoute = require("./routes/questionRoute");
 
 // answer route file
-const answerRoute = require("./routes/answerRoute")
+const answerRoute = require("./routes/answerRoute");
 //reaction route
-const reactionRoute = require("./routes/reactionRoute")
+const reactionRoute = require("./routes/reactionRoute");
 
 // comment route
-const commentRoute = require("./routes/commentRoute")
+const commentRoute = require("./routes/commentRoute");
 
 // json middleware file
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // user route middleware
 app.use("/api/user", userRoute);
 
 // question route middleware
-app.use("/api/question", questionRoute)
+app.use("/api/question", questionRoute);
 
 // answer route middleware
-app.use("/api/question/:question_id/answer", answerRoute)
+app.use("/api/question/:question_id/answer", answerRoute);
 
 // reaction route middleware
-app.use("/api/answer", reactionRoute)
+app.use("/api/answer", reactionRoute);
 
 // comment route middleware
-app.use("/api/answer", commentRoute)
-
-
-
+app.use("/api/answer", commentRoute);
 
 async function start() {
   try {
-    const result = await dbConnection.execute("select 'test' ")
-    await app.listen(port)
+    const result = await dbConnection.execute("select 'test' ");
+    await app.listen(port);
     console.log("Database connection establish");
     console.log("listen on " + port);
   } catch (error) {
     console.log(error.message);
   }
 }
-start()
-
+start();
